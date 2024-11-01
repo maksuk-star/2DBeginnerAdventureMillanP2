a1ab6bf1c1f39794466fe75db346391f8e6457cd
maksuk-star <aedenmillan64@gmail.com>
2024-11-01 09:59:57 -0700

    finished the movment script
    done making the movement script now just need to add more scripts to objects

--- 2DBeginnerAdventureP2Millan/Assets/Scripts/DuckoController.cs
+++ 2DBeginnerAdventureP2Millan/Assets/Scripts/DuckoController.cs
@@ -7,17 +7,21 @@ public class DuckoController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0;
+       // Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        float vertical = Input.GetAxis("Vertical");
+
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
         transform.position = position;
     }
 }
